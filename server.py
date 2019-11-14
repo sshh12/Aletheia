8321bfaee1c706d2f76e000737e55569a60a3843
Shrivu Shankar <shrivu1122@gmail.com>
2019-11-14 11:27:11 -0600

    🔗: Init

---
+++ server.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+BUILD_DIR = os.path.join(os.path.dirname(__file__), 'build')
+
+app = Flask(__name__, static_url_path='', static_folder=BUILD_DIR)
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
