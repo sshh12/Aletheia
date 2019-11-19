7dddd9254e4d143d02eccd60cecdebbd72c9aa00
Shrivu Shankar <shrivu1122@gmail.com>
2019-11-19 15:51:36 -0600

    ✏️: Row Aligned UI

--- src/components/Analyze.js
+++ src/components/Analyze.js
@@ -6,32 +6,54 @@ function heatHSL(val) {
 
 function Analyze({ selected }) {
   console.log(selected);
+  let rowCnt = Math.min(selected[0].section_cnt, selected[1].section_cnt);
   return (
     <div>
       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
         <DocAnalyze doc={selected[0]} showHeader={true} />
         <DocAnalyze doc={selected[1]} />
       </div>
+      {[...Array(rowCnt).keys()].map(idx =>
+        <div style={{ display: 'flex', justifyContent: 'space-around', textAlign: 'left' }}>
+          <RowAnalyze key={idx} docs={selected} idx={idx} />
+        </div>)}
     </div>
   );
 }
 
 function DocAnalyze({ doc, showHeader }) {
-  // let docSents = doc.sections.map(sec => sec.gcp.sent_score * sec.gcp.sent_mag);
-  // let docSent = docSents.reduce((cum, cur) => cum + cur, 0) / docSents.length;
-  // let docQuotes = doc.sections.map(sec => sec.corenlp.quotes).flat();
-  let docSent = 0;
-  // console.log(docQuotes);
-  // console.log(docQuotes.map(x => x.canonicalSpeaker));
+  let sumSent = doc.sections
+    .map(sec => sec.gcp.sent_score * sec.gcp.sent_mag)
+    .reduce((acc, curr) => acc + curr, 0);
+  let docSent = sumSent / doc.sections.length;
   return (
-    <section class="banner style1">
-      <div class="content" style={{height: '100%'}}>
+    <section className="banner style1" style={{ width: '45%' }}>
+      <div className="content" style={{ paddingLeft: '0' }}>
         {showHeader ? <h1>{doc.poem} {doc.book}</h1> : <h1>&nbsp;</h1>}
-        <h1 style={{fontSize: '2em'}}>{doc.translator}</h1>
-        <p class="major">{doc.word_cnt} words</p>
-        <i class="button solid" style={{backgroundColor: heatHSL(docSent * 60)}}>Sentiment: {docSent.toFixed(2)}</i>
-        {doc.sections.map((section, i) => 
-          <blockquote key={i} style={{borderLeftColor: heatHSL(/*section.gcp.sent_score * section.gcp.sent_mag * */60)}}>{section.text}</blockquote>)}
+        <h1 style={{ fontSize: '2em' }}>{doc.translator}</h1>
+        <span className="major">Words: {doc.word_cnt} </span>
+        <br />
+        <span className="major">Sentiment: {docSent.toFixed(2)}</span>
+      </div>
+    </section>
+  );
+}
+
+function RowAnalyze({ idx, docs, key }) {
+  let [docA, docB] = docs;
+  let [secA, secB] = [docA.sections[idx], docB.sections[idx]];
+  return [
+    <TextBlock key={'a' + key} sec={secA} />,
+    <TextBlock key={'b' + key} sec={secB} />
+  ];
+}
+
+function TextBlock({ sec, key }) {
+  let sentiment = sec.gcp.sent_score * sec.gcp.sent_mag * 60;
+  return (
+    <section key={key} keyclassName="style1" style={{ width: '45%' }}>
+      <div className="content">
+        <blockquote style={{ borderLeftColor: heatHSL(sentiment) }}>{sec.text}</blockquote>
       </div>
     </section>
   );
