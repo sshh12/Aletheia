19125bae379ed1a97d1f548313b4f90fd5725e2b
Shrivu Shankar <shrivu1122@gmail.com>
2019-11-19 18:15:27 -0600

    🧰: Adding Build b/c Heroku Lazy

---
+++ build/precache-manifest.8c2830f47f810efd15d9b5d7e9d71526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0909ee260eb96698d89637db425dc66a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55595b1db61e63a99963",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5ab72b1b4aca4d641652",
+    "url": "/static/js/2.62545d5e.chunk.js"
+  },
+  {
+    "revision": "55595b1db61e63a99963",
+    "url": "/static/js/main.769c1993.chunk.js"
+  },
+  {
+    "revision": "dac8044b6694f95b0114",
+    "url": "/static/js/runtime-main.f89149c7.js"
+  }
+]);
